1accfd24a74e9fc7a5c4ad5dc1c95dabe3a79772
dregeta <dmitry.regeta@dataart.com>
2015-07-15 11:08:23 +0300

    - Initial commit to load all data.

---
+++ WpfApplication5/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            SourceList = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                SourceList.Add(i);
+            }
+        }
+
+        public int SelectedItem
+        {
+            get { return (int)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(int), typeof(MainWindow));
+
+        public List<int> SourceList
+        {
+            get { return (List<int>)GetValue(SourceListProperty); }
+            set { SetValue(SourceListProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceListProperty =
+            DependencyProperty.Register("SourceList", typeof(List<int>), typeof(MainWindow));
+
+        private void ScrollIntoView4(object sender, SelectionChangedEventArgs e)
+        {
+            //ScrollIntoView(ListBox.SelectedItem);
+            //ListBox.ScrollToCenterOfView(ListBox.SelectedItem);
+        }
+
+        private void ListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            Debug.Print(string.Format("ListBox_OnSelectionChanged item is : {0}", listBox.SelectedItem));
+            ListBox.ScrollIntoView(ListBox.SelectedItem);
+        }
+
+        private void ListBox_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            
+        }
+
+        private void ListBox_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ListBox.SelectedItem = null;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ListBox.SelectedItem = null;
+        }
+
+    }
+    public class ListBoxScroll : ListBox
+    {
+        public ListBoxScroll()
+            : base()
+        {
+            SelectionChanged += new SelectionChangedEventHandler(ListBoxScroll_SelectionChanged);
+        }
+
+        void ListBoxScroll_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ScrollIntoView(SelectedItem);
+        }
+    }
+
+
+    public static class ItemsControlExtensions
+    {
+        public static void ScrollToCenterOfView(this ItemsControl itemsControl, object item)
+        {
+            // Scroll immediately if possible
+            if (!itemsControl.TryScrollToCenterOfView(item))
+            {
+                // Otherwise wait until everything is loaded, then scroll
+                if (itemsControl is ListBox) ((ListBox)itemsControl).ScrollIntoView(item);
+                itemsControl.Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
+                {
+                    itemsControl.TryScrollToCenterOfView(item);
+                }));
+            }
+        }
+
+        private static bool TryScrollToCenterOfView(this ItemsControl itemsControl, object item)
+        {
+            // Find the container
+            var container = itemsControl.ItemContainerGenerator.ContainerFromItem(item) as UIElement;
+            if (container == null) return false;
+
+            // Find the ScrollContentPresenter
+            ScrollContentPresenter presenter = null;
+            for (Visual vis = container; vis != null && vis != itemsControl; vis = VisualTreeHelper.GetParent(vis) as Visual)
+                if ((presenter = vis as ScrollContentPresenter) != null)
+                    break;
+            if (presenter == null) return false;
+
+            // Find the IScrollInfo
+            var scrollInfo =
+                !presenter.CanContentScroll ? presenter :
+                presenter.Content as IScrollInfo ??
+                FirstVisualChild(presenter.Content as ItemsPresenter) as IScrollInfo ??
+                presenter;
+
+            // Compute the center point of the container relative to the scrollInfo
+            Size size = container.RenderSize;
+            Point center = container.TransformToAncestor((Visual)scrollInfo).Transform(new Point(size.Width / 2, size.Height / 2));
+            center.Y += scrollInfo.VerticalOffset;
+            center.X += scrollInfo.HorizontalOffset;
+
+            // Adjust for logical scrolling
+            if (scrollInfo is StackPanel || scrollInfo is VirtualizingStackPanel)
+            {
+                double logicalCenter = itemsControl.ItemContainerGenerator.IndexFromContainer(container) + 0.5;
+                Orientation orientation = scrollInfo is StackPanel ? ((StackPanel)scrollInfo).Orientation : ((VirtualizingStackPanel)scrollInfo).Orientation;
+                if (orientation == Orientation.Horizontal)
+                    center.X = logicalCenter;
+                else
+                    center.Y = logicalCenter;
+            }
+
+            // Scroll the center of the container to the center of the viewport
+            if (scrollInfo.CanVerticallyScroll) scrollInfo.SetVerticalOffset(CenteringOffset(center.Y, scrollInfo.ViewportHeight, scrollInfo.ExtentHeight));
+            if (scrollInfo.CanHorizontallyScroll) scrollInfo.SetHorizontalOffset(CenteringOffset(center.X, scrollInfo.ViewportWidth, scrollInfo.ExtentWidth));
+            return true;
+        }
+
+        private static double CenteringOffset(double center, double viewport, double extent)
+        {
+            return Math.Min(extent - viewport, Math.Max(0, center - viewport / 2));
+        }
+        private static DependencyObject FirstVisualChild(Visual visual)
+        {
+            if (visual == null) return null;
+            if (VisualTreeHelper.GetChildrenCount(visual) == 0) return null;
+            return VisualTreeHelper.GetChild(visual, 0);
+        }
+    }
+}
